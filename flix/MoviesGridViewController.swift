8dec0bd47e2b60af05e75e02351660507fa2797a
Ajaydip Singh <singh@minerva.kgi.edu>
2021-02-27 09:27:53 +0300

    :bug: fix: spacing in movies grid view

--- flix/MoviesGridViewController.swift
+++ flix/MoviesGridViewController.swift
@@ -28,7 +28,7 @@ class MoviesGridViewController: UIViewController, UICollectionViewDelegate, UICo
         layout.minimumInteritemSpacing = 2
         
         let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2) / 3
-        layout.itemSize = CGSize(width: width, height: width * 3 / 2)
+        layout.estimatedItemSize = CGSize(width: width, height: width * 3 / 2)
 
         // Do any additional setup after loading the view.
         let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
